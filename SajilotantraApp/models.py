edc1f3e4e13901bb5277c3d1fc24417d38fbf807
Ashok <bayes.ashok@gmail.com>
2023-12-04 17:04:35 +0545

    added user

--- SajilotantraApp/models.py
+++ SajilotantraApp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    user_id = models.AutoField(primary_key=True)
+    fName = models.CharField(max_length=200)
+    lName = models.CharField(max_length=200)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=128)
+    
+
+    def __str__(self):
+        return self.user_id
