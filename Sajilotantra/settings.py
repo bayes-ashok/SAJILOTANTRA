b31958f271f2f297eec2bda4eaacded3d7c22202
Ashok <bayes.ashok@gmail.com>
2023-12-09 12:51:09 +0545

    img and jazzmin

--- Sajilotantra/settings.py
+++ Sajilotantra/settings.py
@@ -39,35 +39,15 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'SajilotantraApp',
-    'material',
-    'material.admin',
+    'jazzmin',
+    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
-MATERIAL_ADMIN_SITE = {
-    'HEADER':  'Sajilotantra Admin Panel',  # Admin site header
-    'TITLE': 'Sajilotantra Admin Panel',  # Admin site title
-    # 'FAVICON':  'path/to/favicon',  # Admin site favicon (path to static should be specified)
-    # 'MAIN_BG_COLOR':  'color',  # Admin site main color, css color should be specified
-    # 'MAIN_HOVER_COLOR':  'color',  # Admin site main hover color, css color should be specified
-    'PROFILE_PICTURE':  'assets/logo.png',  # Admin site profile picture (path to static should be specified)
-    # 'PROFILE_BG':  'path/to/image',  # Admin site profile background (path to static should be specified)
-    'LOGIN_LOGO':  'assets/logo.png',  # Admin site logo on login page (path to static should be specified)
-    # 'LOGOUT_BG': 'assets/LogoWhite.png',  # Admin site background on login/logout pages (path to static should be specified)
-    # 'SHOW_THEMES':  True,  #  Show default admin themes button
-    # 'TRAY_REVERSE': True,  # Hide object-tools and additional-submit-line by default
-    # 'NAVBAR_REVERSE': True,  # Hide side navbar by default
-    # 'SHOW_COUNTS': True, # Show instances counts for each model
-    # 'APP_ICONS': {  # Set icons for applications(lowercase), including 3rd party apps, {'application_name': 'material_icon_name', ...}
-    #     'sites': 'send',
-    # },
-    # 'MODEL_ICONS': {  # Set icons for models(lowercase), including 3rd party models, {'model_name': 'material_icon_name', ...}
-    #     'site': 'contact_mail',
-    # }
-}
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
