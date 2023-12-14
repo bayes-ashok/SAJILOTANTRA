b50d33caca97ec9b3e1645cf064f696d9746fdd4
Ashok <bayes.ashok@gmail.com>
2023-12-14 20:25:39 +0545

    ready

--- SajilotantraApp/admin.py
+++ SajilotantraApp/admin.py
@@ -10,7 +10,5 @@ admin.site.register(User)
 admin.site.register(Guidance)
 admin.site.register(Notification)
 from SajilotantraApp.models import User,Event
-# Register your models here.
 
-admin.site.register(User)
 admin.site.register(Event)
