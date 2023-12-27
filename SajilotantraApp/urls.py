11eb342982135d48c2f7c241a292fbd7093be192
Nikesh <bhandarinikesh93@gmail.com>
2023-12-27 10:12:15 +0545

    modified as of sandy

--- SajilotantraApp/urls.py
+++ SajilotantraApp/urls.py
@@ -10,8 +10,11 @@ urlpatterns = [
     path('events', views.events,name='events'),
     # path('play', views.playground,name='playground'),
     path('dashboard', views.dashboard,name='dashboard'),
-    path('card',views.card,name='card'),
     path("activate<uidb64>/<token>",views.activate,name="activate"),
-    path('guide/steps/<int:pk>/<str:category>/', views.guide_steps, name='guide'),
-
+    path('allguides',views.card,name='card'),
+    path('guide/<int:pk>', views.guide_blog, name='guide_blog'),
+    path('government_profiles',views.government_profiles,name='government_profiles'),
+    path('government_profile/<int:pk>', views.government_profiles_details, name='government_profiles_details'),
+    path('all_events/', views.all_events, name='all_events'),
+    path('map/', views.map, name='map'),
 ]
