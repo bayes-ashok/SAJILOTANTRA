260115cbbd073e2a7282e5ec0f9b14305eb3e5e2
Nikesh <bhandarinikesh93@gmail.com>
2023-12-31 11:21:41 +0545

    imported accordingly

--- SajilotantraApp/admin.py
+++ SajilotantraApp/admin.py
@@ -1,17 +1,10 @@
 from django.contrib import admin
 
-from SajilotantraApp.models import User
-
 from .models import GovernmentProfile, Guidance, Notification
 
-# Register your models here.
-
-admin.site.register(User)
 admin.site.register(Guidance)
 admin.site.register(Notification)
 from SajilotantraApp.models import Event, User
 
 admin.site.register(GovernmentProfile)
-
 admin.site.register(Event)
-
