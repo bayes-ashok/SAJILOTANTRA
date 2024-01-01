9de485f73a1c60444c6d9734ec2673d28f364ec0
Nikesh <bhandarinikesh93@gmail.com>
2024-01-01 19:12:42 +0545

    tables for feedback

--- SajilotantraApp/models.py
+++ SajilotantraApp/models.py
@@ -1,9 +1,8 @@
 from ckeditor.fields import RichTextField
-from django.db import models
 from django.contrib.auth.models import User
+from django.db import models
 
-
-
+from Sajilotantra import settings
 
 # class User(models.Model):
 #     user_id = models.AutoField(primary_key=True)
@@ -11,11 +10,12 @@ from django.contrib.auth.models import User
 #     lName = models.CharField(max_length=200)
 #     email = models.EmailField(unique=True)
 #     password = models.CharField(max_length=128)
+#     image = models.ImageField(upload_to='static/profiles/', null=True, blank=True)
+#     bio = models.CharField(max_length=255, null=True, blank=True)
 #     # image = models.ImageField()
 
 #     def __str__(self):
 #         return self.user_id
-
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -27,17 +27,13 @@ class UserProfile(models.Model):
     def __str__(self):
         return self.user.username
 
-
 class GovernmentProfile(models.Model):
+    profile_id=models.AutoField(primary_key=True)
     name= models.CharField(max_length=100)
     thumbnail = models.ImageField(upload_to='static/thumbnails/')
-    description = models.TextField()
+    description = RichTextField() 
     address = models.CharField(max_length=200)
 
-    def __str__(self):
-        return self.name
-    
-    
 class Guidance(models.Model):
     id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=200)
@@ -67,4 +63,15 @@ class Event(models.Model):
     end=models.DateTimeField(null=True,blank=True)
     
     def __str__(self):
-        return self.title
+        return self.name
+    
+class Feedback(models.Model):
+    category = models.CharField(max_length=100)
+    suggestion = models.TextField()
+
+class UploadedFile(models.Model):
+    feedback = models.ForeignKey(Feedback, on_delete=models.CASCADE)
+    file = models.FileField(upload_to='static/feedback_files/')
+
+    def __str__(self):
+        return self.file.name
