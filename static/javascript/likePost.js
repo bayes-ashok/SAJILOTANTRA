4164642a5f1b73696e610f5fc73532bac6ff9563
Nikesh <bhandarinikesh93@gmail.com>
2024-01-18 18:11:45 +0545

    commit before pull

--- static/javascript/likePost.js
+++ static/javascript/likePost.js
@@ -15,12 +15,39 @@ function getCookie(name) {
 }
 
 
-// Function to handle liking a post
+// // Function to handle liking a post
+// function likePost(postId) {
+//     // Retrieve CSRF token
+//     var csrfToken = getCookie('csrftoken');
+
+//     // Use CSRF token in fetch request
+//     fetch(`/post/${postId}/like/`, {
+//         method: 'POST',
+//         headers: {
+//             'X-CSRFToken': csrfToken,
+//             'Content-Type': 'application/json'
+//         },
+//         body: JSON.stringify({ 'post_id': postId })
+//     })
+//     .then(response => response.json())
+//     .then(data => {
+//         if (data.message === 'Post liked successfully') {
+//             let likeCountElement = document.getElementById('like-count-' + postId);
+//             likeCountElement.innerText = parseInt(likeCountElement.innerText) + 1;
+//         } else if (data.is_liked !== undefined) {
+//             // Toggle 'liked' class based on the server response
+//             let likeButton = $('.like-button[data-post-id="' + postId + '"]');
+//             likeButton.toggleClass('liked', data.is_liked);
+//         } else {
+//             alert(data.message); // Handle other messages
+//         }
+//     })
+//     .catch(error => console.error('Error:', error));
+// }
+
 function likePost(postId) {
-    // Retrieve CSRF token
     var csrfToken = getCookie('csrftoken');
 
-    // Use CSRF token in fetch request
     fetch(`/post/${postId}/like/`, {
         method: 'POST',
         headers: {
@@ -31,19 +58,23 @@ function likePost(postId) {
     })
     .then(response => response.json())
     .then(data => {
+        let likeButton = document.querySelector('.like-button[data-post-id="' + postId + '"]');
+
         if (data.message === 'Post liked successfully') {
             let likeCountElement = document.getElementById('like-count-' + postId);
             likeCountElement.innerText = parseInt(likeCountElement.innerText) + 1;
+            likeButton.classList.add('liked');
+            likeButton.classList.remove('not-liked');
         } else if (data.is_liked !== undefined) {
-            // Toggle 'liked' class based on the server response
-            let likeButton = $('.like-button[data-post-id="' + postId + '"]');
-            likeButton.toggleClass('liked', data.is_liked);
+            likeButton.classList.toggle('liked', data.is_liked);
+            likeButton.classList.toggle('not-liked', !data.is_liked);
         } else {
             alert(data.message); // Handle other messages
         }
     })
     .catch(error => console.error('Error:', error));
 }
+
 
 
 // Event listener after DOM content is loaded
