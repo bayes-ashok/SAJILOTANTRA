02b599b503a6ee396e07bddbf8c49f6700de15cc
sandeshs0 <hellosandesh0@gmail.com>
2024-01-28 10:56:46 +0545

    landing url set

--- SajilotantraApp/urls.py
+++ SajilotantraApp/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index,name='index'),
+    path('', views.landing,name='index'),
     path('signup', views.signup,name='signup'),
     path('signin', views.signin,name='signin'),
     # path('admin', views.admin,name='admin'),
